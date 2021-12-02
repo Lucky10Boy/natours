4ef130f7aa914722d90a7c3519106f040c14c148
Usmonalikhon Nizomov <lucky10boypro@gmail.com>
2021-12-02 20:14:10 +0500

    Added a crazy comment

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(cookieParser());
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// asd
 // MIDDLEWARE FOR TIME OF REQUEST
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
